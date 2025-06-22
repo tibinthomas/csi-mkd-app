b946a19159f79d36ab38be6db38c3cd9486d909e
tibinthomas <tibinthomas@carestack.com>
2025-06-22 17:33:05 +0530

    enable swagger

--- Web app/csi-mkd-premarital-app-BE/Program.cs
+++ Web app/csi-mkd-premarital-app-BE/Program.cs
@@ -2,10 +2,17 @@ using csi_mkd_premarital_app_BE.Data;
 using csi_mkd_premarital_app_BE.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new() { Title = "My API", Version = "v1" });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
@@ -15,10 +22,14 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CSI MKD API V1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
@@ -29,8 +40,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapGet("/", () => "Hi!");
-
-app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapGet("/", () => "Hi from Minimal API!").WithOpenApi();
 
 app.Run();
