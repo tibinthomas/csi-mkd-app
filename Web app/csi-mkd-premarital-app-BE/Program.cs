2eafa25afd279739dc42057888a4b4eba2793954
Teena Elsa Thomas <teena496@outlook.com>
2025-08-02 12:42:42 -0400

    feat: update Swagger configuration for development environment

--- Web app/csi-mkd-premarital-app-BE/Program.cs
+++ Web app/csi-mkd-premarital-app-BE/Program.cs
@@ -27,27 +27,29 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.WriteIndented = true;
     });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+if (builder.Environment.IsDevelopment())
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
     {
-        Title = "CSI MKD API",
-        Version = "v1",
-        Description = "API for CSI MKD Premarital Counseling Application"
-    });
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "CSI MKD API",
+            Version = "v1",
+            Description = "API for CSI MKD Premarital Counseling Application"
+        });
 
-    // Add JWT Authentication to Swagger
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
+        // Add JWT Authentication to Swagger
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
 
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
         {
             new OpenApiSecurityScheme
@@ -61,7 +63,8 @@ builder.Services.AddSwaggerGen(options =>
             Array.Empty<string>()
         }
     });
-});
+    });
+}
 
 // Configure CORS
 builder.Services.AddCors(options =>
