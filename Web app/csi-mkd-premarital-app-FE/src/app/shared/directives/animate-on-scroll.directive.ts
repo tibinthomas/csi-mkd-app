7dae4160d4c25dcef57e3709a319835330cae695
tibinthomas <tibinthomas@outlook.com>
2025-08-08 09:26:33 +0530

    add few animatios

--- Web app/csi-mkd-premarital-app-FE/src/app/shared/directives/animate-on-scroll.directive.ts
+++ Web app/csi-mkd-premarital-app-FE/src/app/shared/directives/animate-on-scroll.directive.ts
@@ -1,18 +1,30 @@
-import { Directive, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  AfterViewInit,
+  Input,
+} from '@angular/core';
 
 @Directive({
   selector: '[appAnimateOnScroll]',
+  standalone: true,
 })
 export class AnimateOnScrollDirective implements AfterViewInit {
+  @Input('appAnimateOnScroll') animationType = 'fadeIn';
+
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngAfterViewInit(): void {
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          this.renderer.addClass(this.el.nativeElement, 'animate');
+          this.renderer.addClass(
+            this.el.nativeElement,
+            `animate-${this.animationType}`
+          );
           this.renderer.removeClass(this.el.nativeElement, 'opacity-0');
-          observer.disconnect(); // Optional: animate once only
+          observer.disconnect();
         }
       },
       {
