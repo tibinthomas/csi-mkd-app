dbd0100819b14def7f96770f308f8a08f722fa7f
tibinthomas <tibinthomas@outlook.com>
2025-08-09 18:06:40 +0530

    add deployment token

---
+++ Web app/csi-mkd-premarital-app-FE/deploy.sh
@@ -0,0 +1,6 @@
+
+
+swa deploy ./dist/csi-mkd-counselling/browser \
+  --app-name csi-mkd-premarital-app-fe \
+  --env production \
+  --deployment-token 038de867bb60301592aaa7e8d1868afb13f76b16558515cd89fcb2b967ad3d9002-cfd897b8-72dc-445c-b406-b78647f9d55901e18170a242d21e
