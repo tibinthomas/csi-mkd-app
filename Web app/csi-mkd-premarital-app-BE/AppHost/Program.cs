b59bc4641ad77e7d5a3a98d6c1e350d24ba9a389
tibithomas <tibithomas@outlook.com>
2025-08-16 09:22:12 +0530

    configure aspire

--- Web app/csi-mkd-premarital-app-BE/AppHost/Program.cs
+++ Web app/csi-mkd-premarital-app-BE/AppHost/Program.cs
@@ -1,7 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-// Reference the API project using its assembly name as generated in Projects.g.cs
-builder.AddProject("csi-mkd-premarital-app-BE", "../csi-mkd-premarital-app-BE.csproj")
-       .WithExternalHttpEndpoints();
+// Add the main API project
+var mainApi = builder.AddProject("main-api", "../csi-mkd-premarital-app-BE.csproj")
+    .WithExternalHttpEndpoints();
 
-builder.Build().Run();
+// Add the Sessions Function project  
+var sessionsFunction = builder.AddProject("sessions-function", "../SessionsFunction/SessionsFunction.csproj")
+    .WithExternalHttpEndpoints();
+
+var app = builder.Build();
+app.Run();
