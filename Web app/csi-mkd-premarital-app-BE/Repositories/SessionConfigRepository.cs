89c34e6585678c1223e1c468d201a20942f6265b
Teena Elsa Thomas <teena496@outlook.com>
2025-09-02 11:10:34 -0400

    Commented the deactivate sesssion while fetching the data

--- Web app/csi-mkd-premarital-app-BE/Repositories/SessionConfigRepository.cs
+++ Web app/csi-mkd-premarital-app-BE/Repositories/SessionConfigRepository.cs
@@ -18,44 +18,44 @@ public class SessionConfigRepository : ISessionConfigRepository
 
     public async Task<List<SessionConfiguration>> GetAll()
     {
-        var today = DateTime.UtcNow.Date;
-        var threeDaysFromToday = today.AddDays(3);
-        var hasChanges = false;
+        // var today = DateTime.UtcNow.Date;
+        // var threeDaysFromToday = today.AddDays(3);
+        // var hasChanges = false;
 
-        // Fetch sessions that need to be deactivated (past)
-        var pastSessions = await _context.SessionConfigurations
-            .Where(s => s.StartDate.Date < today && s.IsActive)
-            .ToListAsync();
+        // // Fetch sessions that need to be deactivated (past)
+        // var pastSessions = await _context.SessionConfigurations
+        //     .Where(s => s.StartDate.Date < today && s.IsActive)
+        //     .ToListAsync();
 
-        if (pastSessions.Count > 0)
-        {
-            foreach (var session in pastSessions)
-                session.IsActive = false;
+        // if (pastSessions.Count > 0)
+        // {
+        //     foreach (var session in pastSessions)
+        //         session.IsActive = false;
 
-            hasChanges = true;
-        }
+        //     hasChanges = true;
+        // }
 
-        // Fetch sessions that need to be deactivated (upcoming in next 3 days)
-        var upcomingSessions = await _context.SessionConfigurations
-            .Where(s => s.StartDate.Date >= today
-                        && s.StartDate.Date <= threeDaysFromToday
-                        && s.IsActive)
-            .ToListAsync();
+        // // Fetch sessions that need to be deactivated (upcoming in next 3 days)
+        // var upcomingSessions = await _context.SessionConfigurations
+        //     .Where(s => s.StartDate.Date >= today
+        //                 && s.StartDate.Date <= threeDaysFromToday
+        //                 && s.IsActive)
+        //     .ToListAsync();
 
-        if (upcomingSessions.Count > 0)
-        {
-            foreach (var session in upcomingSessions)
-                session.IsActive = false;
+        // if (upcomingSessions.Count > 0)
+        // {
+        //     foreach (var session in upcomingSessions)
+        //         session.IsActive = false;
 
-            hasChanges = true;
-        }
+        //     hasChanges = true;
+        // }
 
-        // Save only if any updates happened
-        if (hasChanges)
-        {
-            _context.SessionConfigurations.UpdateRange(pastSessions.Concat(upcomingSessions));
-            await _context.SaveChangesAsync();
-        }
+        // // Save only if any updates happened
+        // if (hasChanges)
+        // {
+        //     _context.SessionConfigurations.UpdateRange(pastSessions.Concat(upcomingSessions));
+        //     await _context.SaveChangesAsync();
+        // }
 
         // Now return the latest data using compiled query
         var list = new List<SessionConfiguration>();
