a3a2def1e62d6ab7fcd9b748701bea265572fe71
tibithomas <tibithomas@outlook.com>
2025-09-05 20:56:17 +0530

    fix

--- Web app/csi-mkd-premarital-app-BE/AppHost/Program.cs
+++ Web app/csi-mkd-premarital-app-BE/AppHost/Program.cs
@@ -2,12 +2,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Add the main API project
 var mainApi = builder.AddProject("main-api", "../csi-mkd-premarital-app-BE.csproj")
-    .WithHttpEndpoint(port: 5177, name: "main-api-http")
     .WithExternalHttpEndpoints();
 
 // Add the CSI MKD Functions project  
 var csiMkdFunctions = builder.AddProject("csi-mkd-functions", "../CsiMkdFunctions/CsiMkdFunctions.csproj")
-    .WithHttpEndpoint(port: 7071, name: "functions-http")
     .WithExternalHttpEndpoints();
 
 var app = builder.Build();
