32818ce4ffb3381ae7b6b7cf42e5ec9790a42542
tibithomas <tibithomas@outlook.com>
2025-09-05 20:44:06 +0530

    5177. 7071 port numbers

--- Web app/csi-mkd-premarital-app-BE/AppHost/Program.cs
+++ Web app/csi-mkd-premarital-app-BE/AppHost/Program.cs
@@ -2,10 +2,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Add the main API project
 var mainApi = builder.AddProject("main-api", "../csi-mkd-premarital-app-BE.csproj")
+    .WithHttpEndpoint(port: 5177, name: "http")
     .WithExternalHttpEndpoints();
 
 // Add the CSI MKD Functions project  
 var csiMkdFunctions = builder.AddProject("csi-mkd-functions", "../CsiMkdFunctions/CsiMkdFunctions.csproj")
+    .WithHttpEndpoint(port: 7071, name: "http")
     .WithExternalHttpEndpoints();
 
 var app = builder.Build();
