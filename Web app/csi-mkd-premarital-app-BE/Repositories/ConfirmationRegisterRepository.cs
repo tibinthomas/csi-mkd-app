df508c3d065649876ff8ad4a4b1bfd864dd073fc
Teena Elsa Thomas <teena496@outlook.com>
2025-11-14 22:28:24 -0500

    fix: ensure FindByIdAsync method tracks changes to ConfirmationRegistration

--- Web app/csi-mkd-premarital-app-BE/Repositories/ConfirmationRegisterRepository.cs
+++ Web app/csi-mkd-premarital-app-BE/Repositories/ConfirmationRegisterRepository.cs
@@ -24,6 +24,7 @@ namespace csi_mkd_premarital_app_BE.Repositories
         public async Task<ConfirmationRegistration?> FindByIdAsync(Guid id)
         {
             var registration = await _context.ConfirmationRegistrations
+                .AsTracking()
                 .Include(r => r.Participants)
                 .FirstOrDefaultAsync(r => r.Id == id);
 
